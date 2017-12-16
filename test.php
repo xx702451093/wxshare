d0a0c1662c506b1eb1442bc9ac7f69760009e44a
xuxiao <kingofrockme@163.com>
2017-12-16 11:36:42 +0800

    test strlen

--- test.php
+++ test.php
@@ -8,5 +8,5 @@
 	// include("function.php");
 	// echo getAccessToken();
 	
-	echo strlen("https://mp.weixin.qq.com/s?__biz=MjM5NjI5OTk1NA==&mid=200073159&idx=1&sn=03e4b47922f3dba43b9c598bdae52a3a#rd");
+	echo strlen("gQHx8DwAAAAAAAAAAS5odHRwOi8vd2VpeGluLnFxLmNvbS9xLz");
 ?>
